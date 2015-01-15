5037a3ab8184c1b93dc2ce86519c62e50cbf45b5
Sokolov A <teopetuk@gmail.com>
2015-01-15 13:28:48 +0400

    HG-459: first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+var dnode = require('dnode');
+var domain = require('domain');
+var Promise = require('bluebird');
+
+module.exports = function(opts) {
+  var connection;
+  return new Promise(function(resolve, reject) {
+    var safe, isStop = false;
+    safe = domain.create();
+    safe.on('error', function(e) {
+      connection = undefined;
+      isStop = true;
+      reject(e);
+    });
+    return safe.run(function() {
+      var d = dnode(undefined, opts.options);
+      connection = d.connect(opts.uri);
+      connection.on('error', function(e) {
+        reject(e);
+        if (!isStop) {
+          connection.end();
+          connection = undefined;
+          isStop = true;
+        }
+      });
+      return connection.on('remote', function(remote) {
+        return resolve(Promise.promisifyAll(remote));
+      });
+    });
+  })
+  .timeout(opts.timeout)
+  .disposer(function() {
+    if (connection) {
+      return connection.end();
+    }
+  });
+};
+
